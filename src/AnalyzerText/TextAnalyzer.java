7babfd7b4a1af0b2604157490ff7a3985e319464
kikout13 <kikout13@gmail.com>
2014-03-23 10:26:47 +0700

    2nd commit

--- src/AnalyzerText/TextAnalyzer.java
+++ src/AnalyzerText/TextAnalyzer.java
@@ -1,106 +1,129 @@
 package AnalyzerText;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.Map.Entry;
+
+import com.google.gson.Gson;
 
 import jvntagger.MaxentTagger;
 import jvntagger.POSTagger;
 import vn.hus.nlp.tokenizer.VietTokenizer;
 
 public class TextAnalyzer {
-	public ArrayList<String> str = new ArrayList<String>();
-	//public ArrayList<String> verbArray = new ArrayList<String>();
-	public Map<String, Integer> verbTable = new HashMap<String, Integer>();
-	public static String dateStr;
+	private static final String CONTENT_FILE = "baomoi.content.";
+	private static final String TOKENIZED_FILE = "tokenized.txt";
 	
-	public static final String CONTENT_FILE = "baomoi.content.";
-	public static final String TAGGED_FILE = "tagged.txt";
-	public static final String TOKENIZED_FILE = "tokenized.txt";
-
-	public String inputFile;
-	public String outputFile;
+	private Map<String, Integer> verbTable = new HashMap<String, Integer>();
+	private static String dateStr;
 	
-	public TextAnalyzer (String inputFile, String outputFile) throws IOException {
+	private String inputFile;
+	
+	public TextAnalyzer (String inputFile) throws IOException {
 		this.inputFile = inputFile;
-		this.outputFile = outputFile;
 		
+		//get current Date
 		DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 		Date date = new Date();
 		dateStr = dateFormat.format(date);
 	}
 	
+	public void writeContentToFile(String outputFile) throws IOException{
+		//Open Input, Output stream
+		BufferedReader br = new BufferedReader(new FileReader(inputFile));
+		PrintWriter pw = new PrintWriter(outputFile+dateStr+".txt");
+		
+		String currentLine;
+		String[] token = null;
+		String content;
+		
+		//read line, get content from json, write to output file
+		while ((currentLine = br.readLine()) !=null) {
+			if(!currentLine.equals("\n")) token = currentLine.split("\t");
+			Article art = new Article();
+			Gson gson = new Gson();
+			art = gson.fromJson(token[3], Article.class);
+			content = art.getContent();
+			content = content.replace("\n", "");
+			content = content.replace("<strong>", "");
+			content = content.replace("</strong>", "");
+			pw.println(content);
+			pw.flush();
+		}
+		//Close Input, Output stream
+		br.close();
+		pw.close();
+	}
+	
+	//tokenize content file
 	public void tokenAnalyze() {
 		VietTokenizer tokenizer = new VietTokenizer();
 		tokenizer.tokenize(CONTENT_FILE+dateStr+".txt", TOKENIZED_FILE);
 	}
 	
-	public void tagAnalyze() throws IOException {
+	public void verbAnalyze() throws IOException {
 		String modelDir = "model/maxent";
 		POSTagger tagger = null;
 		tagger = new MaxentTagger(modelDir);
-		String resultStr;
+	
 		BufferedReader br = new BufferedReader(new FileReader(TOKENIZED_FILE));
-		PrintWriter pw = new PrintWriter(TAGGED_FILE);
-		String currentLine;
+		String currentLine, taggedLine;
+		String[] token;
+		
 		while ((currentLine = br.readLine()) !=null) {
 			if(!currentLine.equals("\n")) {
-				resultStr = tagger.tagging(currentLine.toLowerCase());
-				pw.println(resultStr);
-			}
-		}
-		pw.close();
-	}
-	
-	public void verbAnalyze(String filename) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(filename));
-		String currentLine;
-		while ((currentLine = br.readLine()) !=null) {
-			StringTokenizer token = new StringTokenizer(currentLine," ");
-			while (token.hasMoreTokens()) str.add(token.nextToken());
-		}
-		
-		for(int i=0;i<str.size();i++) {
-			String term = str.get(i);
-			if(term.endsWith("/V")) {
-				String verb = term.substring(0, term.length()-2);
+				taggedLine = tagger.tagging(currentLine.toLowerCase());				//tag tokenized word in the line
+				token = taggedLine.split(" ");										//split tagged word in token list	
 				
-				if(verbTable.containsKey(verb)) {
-					verbTable.put(verb, verbTable.get(verb)+1);
-				}
-				else {
-					verbTable.put(verb, 1);
-					//verbArray.add(verb);
+				//get verb and put in verbTable
+				for(int i=0;i<token.length;i++) {
+					if(token[i].endsWith("/V")) {
+						String verb = token[i].substring(0,token[i].length()-2);
+						
+						if(verbTable.containsKey(verb)) {
+							verbTable.put(verb, verbTable.get(verb)+1);
+						}
+						else {
+							verbTable.put(verb, 1);
+						}
+					}
 				}
 			}
 		}
 		br.close();
 	}
 	
-	public void printVerbArray() throws IOException{
+	//Write the most 30 common verb to file  
+	public void writeVerbTableToFile() throws IOException{
 		Map<String, Integer> sorted = sortByValues(verbTable);
 		int count = 0;
 		PrintWriter pw = new PrintWriter("MANHVT_"+dateStr+"_VERB.tsv");
-		for(Map.Entry entry:sorted.entrySet()) {
+		
+		for(Map.Entry entry : sorted.entrySet()) {
 			String verb = (String) entry.getKey();
-			verb = verb.replaceAll("_", " ");
+			verb = verb.replace("_", " ");
 			pw.println(verb + "\t" + changeWord(verb) + "\t" + entry.getValue());
+			pw.flush();
 			count++;
 			if(count==30) break;
 		}
 		pw.close();
 	}
 	
+	//change character 1,3,5,7,9 to UpperCase
 	public String changeWord(String originalWord) {
 		StringBuilder changedWord = new StringBuilder(originalWord);
 		for(int i=0;i<changedWord.length();i+=2) {
-			changedWord.setCharAt(i, Character.toUpperCase(changedWord.charAt(i)));
+			if(i>8) break;
+			else {
+				changedWord.setCharAt(i, Character.toUpperCase(changedWord.charAt(i)));
+			}
 		}
-//		for(int i=0;i<changedWord.length();i++) {
-//			if(changedWord.charAt(i) == "_") changedWord.setCharAt(i, " ");
-//		}
 		return changedWord.toString();
 	}
 	
@@ -132,14 +155,12 @@ public class TextAnalyzer {
 		return sortedMap;
 	}
 
-	public static void main (String[] args) throws IOException {
-		Input ip = new Input("baomoi.tsv");
-		ip.printStr(CONTENT_FILE);
-		TextAnalyzer textAnalyzer = new TextAnalyzer(CONTENT_FILE+dateStr+".txt", TOKENIZED_FILE);
+	public static void main (String[] args) throws IOException {	
+		TextAnalyzer textAnalyzer = new TextAnalyzer("baomoi.tsv");
+		textAnalyzer.writeContentToFile(CONTENT_FILE);
 		textAnalyzer.tokenAnalyze();
-		textAnalyzer.tagAnalyze();
-		textAnalyzer.verbAnalyze(TAGGED_FILE);
-		textAnalyzer.printVerbArray();
+		textAnalyzer.verbAnalyze();
+		textAnalyzer.writeVerbTableToFile();	
 	}	
 	
 }
